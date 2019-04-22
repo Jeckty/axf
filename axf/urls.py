99d7c342bb8500975847a888a0de35807db2f9dc
jeckty <1111@qq.com>
2019-04-22 16:13:11 +0800

    完成注册和登陆界面

--- axf/urls.py
+++ axf/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     re_path(r'^market/(\d+)/(\d+)/(\d+)/$', views.market,name="market"),
     re_path(r'^cart/$', views.cart,name="cart"),
     re_path(r'^mine/$', views.mine,name="mine"),
+    re_path(r'^register/$', views.register,name="register"),
+    re_path(r'^checkuserid/$',views.checkuserid,name="checkuserid"),
+    re_path(r'^quit/$', views.quit,name="quit"),
+    re_path(r'^login/$', views.login,name="login"),
 ]
 #修改一下
--- axf/views.py
+++ axf/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-from .models import Wheel,nav,Mustbuy,Shop,MainShow,FoodTypes,Goods
+from django.shortcuts import render,redirect
+from django.http import JsonResponse
+import time,random
+from .models import Wheel,nav,Mustbuy,Shop,MainShow,FoodTypes,Goods,User
+from django.conf import settings
+import os
+from django.contrib.auth import logout
 
 # Create your views here.
 def home(request):
@@ -41,4 +46,76 @@ def cart(request):
     return render(request,'axf/cart.html',{"title":"购物车"})
 
 def mine(request):
-    return render(request,'axf/mine.html',{"title":"我的"})
+    username=request.session.get("username","未登录")
+    return render(request,'axf/mine.html',{"title":"我的","username":username})
+#注册
+def register(request):
+    if request.method=="POST":
+        userAccount = request.POST.get("userAccount")
+        userPasswd  = request.POST.get("userPasswd")
+        userName    = request.POST.get("userName")
+        userPhone   = request.POST.get("userPhone")
+        userAdderss = request.POST.get("userAdderss")
+        userRank    = 0
+        Token=time.time() + random.randrange(1, 100000)
+        userToken   =str(Token)
+        f=request.FILES["userImg"]
+        userImg=os.path.join(settings.MDEIA_ROOT,userAccount+".png")
+        with open (userImg,"wb") as fp:
+            for date in f.chunks():
+                fp.write(date)
+
+        user=User.createuser(userAccount,userPasswd,userName,userPhone,userAdderss,userImg,userRank,userToken)
+        user.save()
+        request.session["username"] = userName
+        request.session["token"] = userToken
+        return redirect("/mine/")
+    else:
+        return render(request,'axf/register.html',)
+
+#检查用户明是否可用
+def checkuserid(request):
+    userid=request.POST.get("userid")
+    try:
+        user=User.objects.get(userAccount=userid)
+        return JsonResponse({"data":"改用户已经被注册","status":"error"})
+    except User.DoesNotExist as e:
+        return JsonResponse({"data":"可以注册","status":"success"})
+
+#退出登陆
+def quit(request):
+    logout(request)
+    return redirect("/mine/")
+
+#登陆
+from .forms.login import LoginForm
+from django.http import HttpResponse
+def login(request):
+    if request.method == "POST":
+        f = LoginForm(request.POST)
+        if f.is_valid():
+            # 信息格式没多大问题，验证账号和密码的正确性
+            nameid = f.cleaned_data["username"]
+            pswd = f.cleaned_data["passwd"]
+            try:
+                user = User.objects.get(userAccount = nameid)
+                if user.userPasswd != pswd:
+                    return redirect('/login/')
+            except User.DoesNotExist as e:
+                return redirect('/login/')
+
+            #登陆成功
+            token = time.time() + random.randrange(1, 100000)
+            user.userToken = str(token)
+            user.save()
+            request.session["username"] = user.userName
+            request.session["token"] = user.userToken
+            return redirect('/mine/')
+        else:
+            return render(request, 'axf/login.html', {"title": "登陆", "form": f,"error":f.errors})
+    else:
+        f = LoginForm()
+        return render(request, 'axf/login.html', {"title": "登陆","form":f})
+
+
+
